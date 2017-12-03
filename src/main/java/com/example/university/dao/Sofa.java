80b8a55bc5cbeb6790071046708bf9d92d26fe28
Наталия <nata-moroz96@mail.ru>
2017-12-03 19:01:35 +0200

    add sofa

---
+++ src/main/java/com/example/university/dao/Sofa.java
@@ -0,0 +1,28 @@
+package com.example.university.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Wither;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by Наталия on 03.12.2017.
+ */
+@Entity
+@Data
+@Wither
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sofa {
+
+    @Id
+    private int id;
+    private int price;
+    private String brand;
+    private String armrest;
+    private int numOfSeats;
+
+}
