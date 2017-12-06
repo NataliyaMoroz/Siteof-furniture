bbd8fab30278577907ecf1a32579ed16ef3e1326
Sobolvitya <vasobol1995@gmail.com>
2017-12-06 23:23:27 +0200

    add home page

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,15 +5,33 @@ app.config(function($routeProvider){
             templateUrl: '../views/login.html',
             controller: 'loginController'
         })
+        .when('/home/bed', {
+            templateUrl: '../views/bed.html',
+            controller: 'bedController'
+        })
+        .when('/home/sofa', {
+            templateUrl: '../views/sofa.html',
+            controller: 'sofaController'
+        })
+        .when('/home/mattress', {
+            templateUrl: '../views/mattress.html',
+            controller: 'mattressController'
+        })
+        .when('/home', {
+            templateUrl: '../views/home.html',
+            controller: 'homeController'
+        })
         .otherwise(
             {
                 redirectTo: '/'
             }
         );
 }).controller("MainController", ['$scope','$http', '$window', function($scope, $http, $window){
+    $scope.user = undefined;
+    $scope.showUser = false;
+
     $http.get("be/user/currentUser").then(function (response) {
-        console.log(response);
-    }).catch(function (response) {
-        $window.location.href = '/#!/login';
-    })
+        $scope.user = response.data;
+        $scope.showUser = true;
+    }).catch(function (response) {})
 }]);
--- src/main/resources/static/js/controllers/loginController.js
+++ src/main/resources/static/js/controllers/loginController.js
@@ -1,12 +1,12 @@
-app.controller('loginController', ["$http", "$scope", "$location",
-    function ($http, $scope, $location) {
+app.controller('loginController', ["$http", "$scope", "$window",
+    function ($http, $scope, $window) {
         $scope.username = undefined;
         $scope.password = undefined;
         $scope.canProceed = true;
         
         $scope.login = function () {
             return $http.post("be/user/login", {email:$scope.username, hashPassword:$scope.password}).then(function () {
-                $location.path("/")
+                $window.location.href = '/';
             }).catch(function () {
                 $scope.canProceed = false;
             })
