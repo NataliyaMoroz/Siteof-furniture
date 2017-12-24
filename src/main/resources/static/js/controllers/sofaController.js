293d05ac5b37ae2ea18db73db06514e157bc8065
Наталия <nata-moroz96@mail.ru>
2017-12-24 04:24:38 +0200

    fix filters frontend

--- src/main/resources/static/js/controllers/sofaController.js
+++ src/main/resources/static/js/controllers/sofaController.js
@@ -7,7 +7,16 @@ app.controller('sofaController', ["$http", "$scope", "$window",
             });
         $scope.sofas = [];
 
-        $scope.siteFilter = function ($http, $scope, $window) {
+
+        $("input[type='checkbox']").click(function(){
+            $scope.siteFilter();
+        });
+
+        $("input[type='text']").click(function(){
+            $scope.siteFilter();
+        });
+
+        $scope.siteFilter = function () {
             console.log($scope.priceFrom);
             console.log($scope.priceTo);
             console.log($scope.armrest);
@@ -18,19 +27,101 @@ app.controller('sofaController', ["$http", "$scope", "$window",
             console.log($scope.brandSofyno);
 
             $scope.sofas = [];
-            $http.get("be/sofa/all?priceFrom" + $scope.priceFrom + "?priceTo" +$scope.priceTo
-                + "?brand" + $scope.brandIKEA + "," + $scope.brandSkySoft + "," + $scope.brandLaretto + ","
-                + $scope.brandDivanPlus + "," + $scope.brandSofyno + "?armrest" + $scope.armrest)
+            var priceFromPath = "";
+            var priceToPath = "";
+            var brandPath = "";
+            var armrestPath = "";
+            var fullPath = "";
+            if($scope.priceFrom != undefined) {
+                priceFromPath = "priceFrom=" + $scope.priceFrom;
+            }
+            if($scope.priceTo != undefined) {
+                priceToPath = "priceTo=" + $scope.priceTo;
+            }
+            if($scope.brandIKEA == true) {
+                if(brandPath != "") {
+                    brandPath += "," + "IKEA";
+                }
+                else {
+                    brandPath = "brand=" + "IKEA";
+                }
+            }
+            if($scope.brandSkySoft == true) {
+                if(brandPath != "") {
+                    brandPath += "," + "SkySoft";
+                }
+                else {
+                    brandPath = "brand=" + "SkySoft";
+                }
+            }
+            if($scope.brandLaretto == true) {
+                if(brandPath != "") {
+                    brandPath += "," + "Laretto";
+                }
+                else {
+                    brandPath = "brand=" + "Laretto";
+                }
+            }
+            if($scope.brandDivanPlus == true) {
+                if(brandPath != "") {
+                    brandPath += "," + "DivanPlus";
+                }
+                else {
+                    brandPath = "brand=" + "DivanPlus";
+                }
+            }
+            if($scope.brandSofyno == true) {
+                if(brandPath != "") {
+                    brandPath += "," + $scope.brandSofyno;
+                }
+                else {
+                    brandPath = "brand=" + $scope.brandSofyno;
+                }
+            }
+            if($scope.armrest != undefined || $scope.armrest != all) {
+                armrestPath = "priceFrom=" + $scope.armrest;
+            }
+
+            if (priceFromPath != "") {
+                if (fullPath != "") {
+                    fullPath += "&" + priceFromPath;
+                }
+                else {
+                    fullPath = priceFromPath;
+                }
+            }
+            if (priceToPath != "") {
+                if (fullPath != "") {
+                    fullPath += "&" + priceToPath;
+                }
+                else {
+                    fullPath = priceToPath;
+                }
+            }
+            if (brandPath != "") {
+                if (fullPath != "") {
+                    fullPath += "&" + brandPath;
+                }
+                else {
+                    fullPath = brandPath;
+                }
+            }
+            if (armrestPath != "") {
+                if (fullPath != "") {
+                    fullPath += "&" + armrestPath;
+                }
+                else {
+                    fullPath = armrestPath;
+                }
+            }
+            console.log(priceFromPath);
+            console.log(priceToPath);
+            console.log(armrestPath);
+            console.log(brandPath);
+            console.log(fullPath);
+            $http.get("be/sofa/all?" + fullPath)
                 .then(function (response) {
                     $scope.sofas =  response.data;
                 });
-        }
-
-        $("input[type='checkbox']").click(function(){
-            siteFilter();
-        });
-
-        $("input[type='text']").click(function(){
-            siteFilter();
-        });
+        };
     }]);
